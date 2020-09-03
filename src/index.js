1edeb71c7262cfc094f539a92e6e074f0304fbe6
ola <kveola13@student.westerdals.no>
2020-09-03 08:41:34 +0200

    change to rendering

--- src/index.js
+++ src/index.js
@@ -10,13 +10,28 @@ const user = {
   lastName: "Kveli"
 }
 
-const element = (
+const nameElement = (
   <h1>
     Hello, {formatName(user)}
   </h1>
 )
 
+console.log(nameElement)
+
+function tick() {
+  const element = (
+    <div>
+      <h1>
+        
+      </h1>
+      <h2>
+
+      </h2>
+    </div>
+  )
+}
+
 ReactDOM.render(
-    element,
+    tick.element,
     document.getElementById('root')
 )
