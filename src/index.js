3a76744d3a97a0ac496389ea75a25f4a69014702
ola <kveola13@student.westerdals.no>
2020-09-03 09:39:03 +0200

    prop tut

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import ReactDOM from 'react-dom';
 import React from 'react'
 
+
 function Welcome(props){
   return <h1>Hello, {props.name}</h1>
 }
+/*
+---------
+These are equal
+---------
+
+class Welcome extends React.Component {
+  render(){
+  return <h1>Hello, {this.props.name}</h1>
+  }
+}*/
+
+const element = <Welcome name="Ola Fredrik"/>
+
+ReactDOM.render(
+  element,
+  document.getElementById("root")
+)
