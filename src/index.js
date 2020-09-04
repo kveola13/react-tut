983ccc3424bc100075f30ee70e005dcc8e74721a
ola <kveola13@student.westerdals.no>
2020-09-04 08:19:38 +0200

    Refactoring

--- src/index.js
+++ src/index.js
@@ -28,7 +28,20 @@ function App(){
   )
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-)
+function Clock(props) {
+  return(
+      <div>
+          <h1>Hello, World</h1>
+          <h2>It is {props.date.toLocaleTimeString()}.</h2>
+      </div>
+  )
+}
+
+function tick(){
+  ReactDOM.render(
+      <Clock date={new Date()}/>,
+      document.getElementById('root')
+  )
+}
+
+setInterval(tick, 1000)
