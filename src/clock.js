97d3a5e587cd3beead671a7fba95e5461055d483
ola <kveola13@student.westerdals.no>
2020-09-04 08:19:27 +0200

    Added clock

---
+++ src/clock.js
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom';
+import React from 'react'
+
+function Clock(props) {
+    return(
+        <div>
+            <h1>Hello, World</h1>
+            <h2>It is {props.date.toLocaleTimeString()}.</h2>
+        </div>
+    )
+}
+
+function tick(){
+    ReactDOM.render(
+        <Clock date={new Date()}/>,
+        document.getElementById('root')
+    )
+}
+
+setInterval(tick, 1000)
