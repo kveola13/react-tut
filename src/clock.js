2870f908be4ecfff2bf94724c0f96fabad482c2a
ola <kveola13@student.westerdals.no>
2020-09-05 14:35:56 +0200

    refactor

--- src/clock.js
+++ src/clock.js
@@ -9,12 +9,26 @@ class Clock extends React.Component {
         }
     }
 
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    }
+
+    tick(){
+        this.setState({
+            date: new Date()
+        })
+    }
+
     render(){
         return(
             <div>
-                <h1>
-                    Hello, World!
-                </h1>
                 <h2>
                     It is {this.state.date.toLocaleTimeString()}.
                 </h2>
