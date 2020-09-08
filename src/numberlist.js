e101ab383c259fc03a239863e629ec902aaaa7e8
ola <kveola13@student.westerdals.no>
2020-09-08 10:11:14 +0200

    added key values

--- src/numberlist.js
+++ src/numberlist.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+function ListItem(props){
+    return <li>{props.value}</li>
+}
+
 function NumberList(props){
     const numbers = props.numbers
     const listItems = numbers.map((number)=> 
-        <li>{number}</li>
+        <ListItem key={number.toString()} value={number}></ListItem>
     )
     return (
         <ul>{listItems}</ul>
