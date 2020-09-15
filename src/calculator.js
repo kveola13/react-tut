fb717ad80acc7bc06933cfa3e8c2f31cc409801e
ola <kveola13@student.westerdals.no>
2020-09-15 09:02:37 +0200

    refactor

--- src/calculator.js
+++ src/calculator.js
@@ -1,28 +1,13 @@
 import React from 'react'
+import TemperatureInput from './temperatureinput'
 
 class Calculator extends React.Component{
-    constructor(props){
-        super(props)
-        this.handleChange = this.handleChange.bind(this)
-        this.state={
-            temperature: ''
-        }
-    }
-
-    handleChange(event){
-        this.setState({temperature: event.target.value})
-    }
     render(){
-        const temperature = this.state.temperature
         return(
-            <fieldset>
-                <legend>Enter temperature in Celsius:</legend>
-                <input
-                value={temperature}
-                onChange={this.handleChange}/>
-                <BoilingVerdict
-                celsius={parseFloat(temperature)}/>
-            </fieldset>
+            <div>
+                <TemperatureInput scale="c" />
+                <TemperatureInput scale="f" />
+            </div>
         )
     }
 }
