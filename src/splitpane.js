0a62a7b47261054915bddb58537287d5c0ef7c9b
ola <kveola13@student.westerdals.no>
2020-09-16 10:19:04 +0200

    added splitpane

---
+++ src/splitpane.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+function Contacts(){
+    return <div className="Contacts"></div>
+}
+
+function Chat(){
+    return <div className="Chat"></div>
+}
+
+function SplitPane(props){
+    return(
+        <div className="SplitPane">
+            <div className="SplitPane-left">
+                {props.left}
+            </div>
+            <div className="SplitPane-right">
+                {props.right}
+            </div>
+        </div>
+    )
+}
+function App(){
+    return(
+        <SplitPane
+        left={
+            <Contacts />
+        }
+        right={
+            <Chat/>
+        }
+        />
+    )
+}
